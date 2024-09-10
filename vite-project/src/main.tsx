d83a638d9183b7e9cd8b2ba678f5292608c3358d
yamircolli25 <ycolli.24431@umsa.edu.mx>
2024-09-10 01:31:13 -0600

    .

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react'
+import ReactDoom from 'react-dom/client'
+import Calculadora_Matrices from './Calcculadora_Matrices'
+//import Calculadora from './Calculadora.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+ReactDoom.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Calculadora_Matrices />
+  </React.StrictMode>,
 )
